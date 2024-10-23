7d04e0dea5a08d98acea863a2e3bec8edeeaff22
khanh-phan15 <phank286@newschool.edu>
2024-10-23 16:55:24 -0400

    entry-6-update

---
+++ css/animation.js
@@ -0,0 +1,8 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    const box1 = document.querySelector('.box1');
+    const box2 = document.querySelector('.box2');
+
+    // Add the 'animate' class to both boxes
+    div5.add('animate');
+    div6.add('animate');
+});
